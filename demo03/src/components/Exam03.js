515bdf3861c7e2585f3bf1785c04e6d48aef98fb
LcsCho <chojunhee98@gmail.com>
2023-10-30 17:55:17 +0900

    react

---
+++ demo03/src/components/Exam03.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+
+// function Exam03() {};
+// const Exam03 = function(){};
+const Exam03 = ()=> {
+    const [money, setMoney] = useState(0);
+    
+    return (
+        <>
+            <h1>세 번째 예제</h1>
+            <span>출금금액: {money}원</span>
+            <br/>
+            <button className='btn btn-dark ms-2' onClick={()=>setMoney(money + 100000)}>10만원</button>
+            <button className='btn btn-dark ms-2' onClick={()=>setMoney(money + 50000)}>5만원</button>
+            <button className='btn btn-dark ms-2' onClick={()=>setMoney(money + 10000)}>1만원</button>
+            <button className='btn btn-dark ms-2' onClick={()=>setMoney(0)}>초기화</button>
+            <br/>
+        </>
+    );
+}
+
+export default Exam03;
