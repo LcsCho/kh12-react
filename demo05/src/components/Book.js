7b45baa283daae798f4a5fa42b2f44d970489f1a
LcsCho <chojunhee98@gmail.com>
2023-11-07 11:19:08 +0900

    react

--- demo05/src/components/Book.js
+++ demo05/src/components/Book.js
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
-import { FaXmark } from "react-icons/fa6";
-import { FaRegEdit } from "react-icons/fa";
+import {useState, useEffect, useRef} from "react";
 import axios from "axios";
 
-import "./Book.css";
+import {LiaEdit} from "react-icons/lia";
+import {AiFillDelete, AiOutlinePlus} from "react-icons/ai";
+import { Modal } from "bootstrap";
 
-const Book = (props) => {
+const Book = (props)=>{
     const [bookList, setBookList] = useState([]);
 
-    const loadBook = () => {
+    const loadBook = ()=>{
         axios({
             url:"http://localhost:8080/book/",
             method:"get"
@@ -17,77 +17,260 @@ const Book = (props) => {
             setBookList(response.data);
         })
         .catch(err=>{});
-    }
-    useEffect(() => {
+    };
+
+    useEffect(()=>{
         loadBook();
     }, []);
 
     const deleteBook = (book) => {
+        const choice = window.confirm("정말 삭제하시겠습니까?");
+        if(choice === false) return;
+
         axios({
             url:`http://localhost:8080/book/${book.bookId}`,
             method:"delete"
         })
         .then(response=>{
-            loadBook();
+            loadBook();//목록 갱신
         })
         .catch(err=>{});
-      };
-    
-    const editBook = (book) => {
-
-
     };
-    
+
+    const bsModal = useRef();
+    const openModal = ()=>{
+        const modal = new Modal(bsModal.current);
+        modal.show();
+    };
+    const closeModal = ()=>{
+        const modal = Modal.getInstance(bsModal.current);
+        modal.hide();
+
+        clearBook();
+    };
+
+    const [book, setBook] = useState({
+        bookTitle:"",
+        bookAuthor:"",
+        bookPublicationDate:"",
+        bookPrice:"",
+        bookPublisher:"",
+        bookPageCount:"",
+        bookGenre:"",
+    });
+    const changeBook = (e)=>{
+        setBook({
+            ...book,
+            [e.target.name] : e.target.value
+        });
+    };
+    const clearBook = ()=>{
+        setBook({
+            bookTitle:"",
+            bookAuthor:"",
+            bookPublicationDate:"",
+            bookPrice:"",
+            bookPublisher:"",
+            bookPageCount:"",
+            bookGenre:"",
+        });
+    };
+
+    //axios로 서버에 등록 요청을 보낸 뒤 등록이 성공하면 목록을 갱신하도록 처리
+    const saveBook = ()=>{
+        //입력값 검사 후 차단 코드 추가
+
+        axios({
+            url:"http://localhost:8080/book/",
+            method:"post",
+            data:book
+        })
+        .then(response=>{//성공했다면
+            loadBook();//목록을 갱신하고
+            closeModal();//모달을 닫아라
+        })
+        .catch(err=>{});    
+    };
+
+    const editBook = (target)=>{
+        setBook({...target});
+        openModal();
+    };
+
+    const updateBook = ()=>{
+
+        const {bookId, bookTitle, bookAuthor, bookPublicationDate, 
+            bookPrice, bookPublisher, bookPageCount, bookGenre
+        } = book;
+        axios({
+            url:`http://localhost:8080/book/${bookId}`,
+            method:"put",
+            data:{
+                bookTitle: bookTitle,
+                bookAuthor: bookAuthor,
+                bookPublicationDate: bookPublicationDate,
+                bookPrice: bookPrice,
+                bookPublisher: bookPublisher,
+                bookPageCount: bookPageCount,
+                bookGenre: bookGenre
+            }
+        })
+        .then(response=>{
+            loadBook();
+            closeModal();
+        })
+        .catch(err=>{});
+    };
 
     return (
         <>
+            
             <div className="row">
                 <div className="col">
-                    <h1>도서 목록</h1>
+                    <h1>도서 관리</h1>
                     <p>React CRUD 연습 예제</p>
                 </div>
             </div>
 
+            {/* 추가 버튼 */}
+            <div className="row mt-4">
+                <div className="col text-end">
+                    <button className="btn btn-success" onClick={openModal}>
+                        <AiOutlinePlus/>
+                        추가
+                    </button>
+                </div>
+            </div>
+            
+            {/* 출력 위치 */}
             <div className="row mt-4">
                 <div className="col">
-                    <table class="table">
+                    <table className="table">
                         <thead>
                             <tr>
-                                <th className="pc-only">번호</th>
+                                <th>도서번호</th>
                                 <th>제목</th>
                                 <th>저자</th>
-                                <th className="pc-only">출판일</th>
-                                <th>출판사</th>
+                                <th>출간일</th>
                                 <th>판매가</th>
-                                <th className="pc-only">페이지</th>
-                                <th className="pc-only">장르</th>
-                                <th>수정</th>
-                                <th>삭제</th>
+                                <th>출판사</th>
+                                <th>페이지</th>
+                                <th>장르</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            {bookList.map(book => (
+                            {bookList.map(book=>(
                                 <tr key={book.bookId}>
-                                    <td className="pc-only">{book.bookId}</td>
+                                    <td>{book.bookId}</td>
                                     <td>{book.bookTitle}</td>
                                     <td>{book.bookAuthor}</td>
-                                    <td className="pc-only">{book.bookPublicationDate}</td>
+                                    <td>{book.bookPublicationDate}</td>
                                     <td>{book.bookPublisher}</td>
-                                    <td className="pc-only">{book.bookPrice}</td>
-                                    <td className="pc-only">{book.bookPageCount}</td>
+                                    <td>{book.bookPrice}</td>
+                                    <td>{book.bookPageCount}</td>
                                     <td>{book.bookGenre}</td>
-                                    <td><FaRegEdit className="text-warning ms-1" onClick={e => editBook(book)} />
+                                    <td>
+                                        {/* 아이콘 자리 */}
+                                        <LiaEdit className="text-warning"
+                                            onClick={e=>editBook(book)}/>
+                                        <AiFillDelete className="text-danger" 
+                                            onClick={e=>deleteBook(book)}/>                                        
                                     </td>
-                                    <td><FaXmark className="text-danger ms-1" onClick={e => deleteBook(book)} />
-                                    </td>
-                                    <td></td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
                 </div>
             </div>
+
+            {/* Modal */}
+            <div className="modal fade" ref={bsModal} 
+                        data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" >
+                            {book.bookId === undefined ? 
+                                '신규 도서 등록' : `${book.bookId}번 도서 수정`}
+                        </h5>
+                        <button type="button" className="border-0 bg-transparent" 
+                                                                                    onClick={closeModal}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+
+                        <div className="row">
+                            <div className="col">
+                                <label className="form-label">제목</label>
+                                <input type="text" name="bookTitle" className="form-control"
+                                        value={book.bookTitle} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">저자</label>
+                                <input type="text" name="bookAuthor" className="form-control"
+                                        value={book.bookAuthor} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">출간일</label>
+                                <input type="date" name="bookPublicationDate" className="form-control"
+                                        value={book.bookPublicationDate} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">판매가</label>
+                                <input type="number" name="bookPrice" className="form-control"
+                                        value={book.bookPrice} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">출판사</label>
+                                <input type="text" name="bookPublisher" className="form-control"
+                                        value={book.bookPublisher} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">페이지</label>
+                                <input type="number" name="bookPageCount" className="form-control"
+                                        value={book.bookPageCount} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">장르</label>
+                                <input type="text" name="bookGenre" className="form-control"
+                                        value={book.bookGenre} onChange={changeBook}/>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
+                        {book.bookId === undefined ? 
+                            <button className="btn btn-success" onClick={saveBook}>저장</button>
+                            : 
+                            <button className="btn btn-success" onClick={updateBook}>수정</button>
+                        }
+                        
+                    </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 };
--- demo05/src/components/Pokemon.js
+++ demo05/src/components/Pokemon.js
@@ -1,74 +1,119 @@
-import { useState, useEffect, useRef } from "react";
-import { FaXmark } from "react-icons/fa6";
-import { FaRegEdit } from "react-icons/fa";
-import { AiOutlinePlus, AiFillDelete } from "react-icons/ai";
-import { Modal } from "bootstrap";
+import {useState, useEffect, useRef} from "react";
 import axios from "axios";
 
-const Pokemon = (props) => {
+import {LiaEdit} from "react-icons/lia";
+import {AiFillDelete, AiOutlinePlus} from "react-icons/ai";
+import { Modal } from "bootstrap";
+
+const Pokemon = (props)=>{
     const [pokemonList, setPokemonList] = useState([]);
 
     //서버에서 pokemon list를 불러와서 state에 설정하는 코드
-    const loadPokemon = () => {
+    const loadPokemon = ()=>{
         axios({
-            url: "http://localhost:8080/pokemon/",
-            method: "get"
+            url:"http://localhost:8080/pokemon/",
+            method:"get"
         })
-            .then(response => {
-                //console.log(response);
-                setPokemonList(response.data);
-            })
-            .catch(err => { });
+        .then(response=>{
+            //console.log(response);
+            setPokemonList(response.data);
+        })
+        .catch(err=>{});
     };
 
-
-    useEffect(() => {
+    useEffect(()=>{
         loadPokemon();
     }, []);
 
-    // 포켓몬스터 삭제
-    // - 이제는 state에서 삭제하는 것이 아니라 서버에 통신을 보낸 뒤 목록을 갱신하면 된다
+    //포켓몬스터 삭제
+    //- 이제는 state에서 삭제하는 것이 아니라 서버에 통신을 보낸 뒤 목록을 갱신하면 된다
     const deletePokemon = (pokemon) => {
-        // axios({옵션}).then(성공시 실행할 함수).cath(실패시 실행할 함수);
+        const choice = window.confirm("정말 삭제하시겠습니까?");
+        if(choice === false) return;
+
+        //axios({옵션}).then(성공시 실행할 함수).catch(실패시 실행할 함수);
         axios({
-            url: `http://localhost:8080/pokemon/${pokemon.no}`,
-            method: "delete"
+            url:`http://localhost:8080/pokemon/${pokemon.no}`,
+            method:"delete"
         })
-            .then(response => {
-                loadPokemon(); // 목록 갱신
-            })
-            .catch(err => { });
+        .then(response=>{
+            loadPokemon();//목록 갱신
+        })
+        .catch(err=>{});
     };
 
-    const eidtPokemon = (pokemon) => {
-
-    };
-
-    // modal 관련된 처리
+    //modal 관련된 처리
     const bsModal = useRef();
-    const openModal = () => {
+    const openModal = ()=>{
         const modal = new Modal(bsModal.current);
         modal.show();
     };
-    const closeModal = () => {
+    const closeModal = ()=>{
         const modal = Modal.getInstance(bsModal.current);
         modal.hide();
+
+        clearPokemon();
     };
 
-    // 등록과 관련된 state
-    const [pokemon, setPokemon] = useState({
-        name:"",
-        type:""
-    });
-    const changePokemon = (e) => {
+    //등록과 관련된 state
+    const [pokemon, setPokemon] = useState({name:"" , type:""});
+    const changePokemon = (e)=>{
         setPokemon({
             ...pokemon,
-            [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
         });
+    };
+    const clearPokemon = ()=>{
+        setPokemon({name:"", type:""});
+    };
+
+    //axios로 서버에 등록 요청을 보낸 뒤 등록이 성공하면 목록을 갱신하도록 처리
+    const savePokemon = ()=>{
+        //입력값 검사 후 차단 코드 추가
+
+        axios({
+            url:"http://localhost:8080/pokemon/",
+            method:"post",
+            data:pokemon
+        })
+        .then(response=>{//성공했다면
+            loadPokemon();//목록을 갱신하고
+            closeModal();//모달을 닫아라
+        })
+        .catch(err=>{});    
+    };
+
+    //포켓몬스터 수정 창 열기
+    //- target은 수정 버튼을 누른 행의 포켓몬스터 정보
+    //- target의 정보를 pookemon으로 카피 후 모달 열기
+    const editPokemon = (target)=>{
+        setPokemon({...target});
+        openModal();
+    };
+
+    //포켓몬스터 수정 처리
+    const updatePokemon = ()=>{
+        //검사 후 차단 처리
+
+        const {no, name, type} = pokemon;
+        axios({
+            url:`http://localhost:8080/pokemon/${no}`,
+            method:"put",
+            data:{
+                name : name, 
+                type : type
+            }
+        })
+        .then(response=>{
+            loadPokemon();
+            closeModal();
+        })
+        .catch(err=>{});
     };
 
     return (
         <>
+            
             <div className="row">
                 <div className="col">
                     <h1>포켓몬스터 관리</h1>
@@ -78,18 +123,18 @@ const Pokemon = (props) => {
 
             {/* 추가 버튼 */}
             <div className="row mt-4">
-                <div className="col">
+                <div className="col text-end">
                     <button className="btn btn-success" onClick={openModal}>
-                        <AiOutlinePlus />
+                        <AiOutlinePlus/>
                         추가
                     </button>
                 </div>
             </div>
-
+            
             {/* 출력 위치 */}
             <div className="row mt-4">
                 <div className="col">
-                    <table class="table">
+                    <table className="table">
                         <thead>
                             <tr>
                                 <th>번호</th>
@@ -99,16 +144,18 @@ const Pokemon = (props) => {
                             </tr>
                         </thead>
                         <tbody>
-                            {pokemonList.map(pokemon => (
+                            {pokemonList.map(pokemon=>(
                                 <tr key={pokemon.no}>
                                     <td>{pokemon.no}</td>
                                     <td>{pokemon.name}</td>
                                     <td>{pokemon.type}</td>
-                                    <td><FaRegEdit className="text-warning ms-1" />
+                                    <td>
+                                        {/* 아이콘 자리 */}
+                                        <LiaEdit className="text-warning"
+                                            onClick={e=>editPokemon(pokemon)}/>
+                                        <AiFillDelete className="text-danger" 
+                                            onClick={e=>deletePokemon(pokemon)}/>                                        
                                     </td>
-                                    <td><FaXmark className="text-danger ms-1" onClick={e => deletePokemon(pokemon)} />
-                                    </td>
-                                    <td></td>
                                 </tr>
                             ))}
                         </tbody>
@@ -117,41 +164,51 @@ const Pokemon = (props) => {
             </div>
 
             {/* Modal */}
-            <div className="modal fade" ref={bsModal}
-                data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-hidden="true">
+            <div className="modal fade" ref={bsModal} 
+                        data-bs-backdrop="static" tabIndex="-1" role="dialog" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">제목</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="row">
-                                <div className="col">
-                                    <label className="form-label">이름</label>
-                                    <input type="text" name="name" className="form-control" 
-                                    value={pokemon.name} onChange={changePokemon}/>
-                                </div>
-                            </div>
+                    <div className="modal-header">
+                        <h5 className="modal-title" >
+                            {pokemon.no === undefined ? 
+                                '신규 몬스터 등록' : `${pokemon.no}번 몬스터 수정`}
+                        </h5>
+                        <button type="button" className="border-0 bg-transparent" 
+                                                                                    onClick={closeModal}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
 
-                            <div className="row">
-                                <div className="col">
-                                    <label className="form-label">속성</label>
-                                    <input type="text" name="type" className="form-control" 
-                                    vlaue={pokemon.type} onChange={changePokemon}/>
-                                </div>
+                        <div className="row">
+                            <div className="col">
+                                <label className="form-label">이름</label>
+                                <input type="text" name="name" className="form-control"
+                                        value={pokemon.name} onChange={changePokemon}/>
                             </div>
                         </div>
-                        <div className="modal-footer">
-                            <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
-                            <button className="btn btn-success">저장</button>
+
+                        <div className="row mt-4">
+                            <div className="col">
+                                <label className="form-label">속성</label>
+                                <input type="text" name="type" className="form-control"
+                                        value={pokemon.type} onChange={changePokemon}/>
+                            </div>
                         </div>
+
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-secondary" onClick={closeModal}>닫기</button>
+                        {pokemon.no === undefined ? 
+                            <button className="btn btn-success" onClick={savePokemon}>저장</button>
+                            : 
+                            <button className="btn btn-success" onClick={updatePokemon}>수정</button>
+                        }
+                        
+                    </div>
                     </div>
                 </div>
             </div>
-
 
         </>
     );
